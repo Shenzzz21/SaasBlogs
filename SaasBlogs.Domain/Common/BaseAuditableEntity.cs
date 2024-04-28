98ab7edc517422f56eae4211f3805ad9509657ba
KcSumanKc <kcsuman1203@gamil.com>
2024-04-28 14:28:03 +0545

    Create BaseEntity and BaseAuditableEntity.

---
+++ SaasBlogs.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Domain.Common
+{
+    public abstract class BaseAuditableEntity : BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
