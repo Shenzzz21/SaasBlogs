fc4fb11e5dfbfe9e4300e19999abe51cc241201e
Awix002 <abipradhanang2014@gmail.com>
2024-04-29 12:41:44 +0545

    Add remaining configuration

---
+++ SaasBlogs.Infrastructure/Persistence/EntityConfiguration/NotificationConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SaasBlogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Infrastructure.Persistence.EntityConfiguration
+{
+    internal class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.HasKey(n => n.Id);
+
+            builder.Property(n => n.NotificationContent)
+                .IsRequired();
+
+            builder.Property(n => n.CreatedDate)
+                .IsRequired();
+
+            builder.Property(n => n.ModifiedDate)
+                .IsRequired(false);
+
+            builder.HasOne(n => n.User)
+                .WithMany(u => u.Notifications)
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(n => n.Blog)
+                .WithMany(b => b.Notifications)
+                .HasForeignKey(n => n.BlogId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ SaasBlogs.Infrastructure/Persistence/EntityConfiguration/VoteConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SaasBlogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Infrastructure.Persistence.EntityConfiguration
+{
+    internal class VoteConfiguration : IEntityTypeConfiguration<Vote>
+    {
+        public void Configure(EntityTypeBuilder<Vote> builder)
+        {
+            builder.HasKey(v => v.Id);
+
+            builder.Property(v => v.IsUpvote).IsRequired();
+
+            builder.Property(v => v.CreatedDate)
+                .IsRequired();
+
+            builder.Property(v => v.ModifiedDate)
+                .IsRequired(false);
+
+            builder.HasOne(v => v.User)
+           .WithMany(u => u.Votes)
+           .HasForeignKey(v => v.UserId)
+           .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(v => v.Blog)
+            .WithMany(b => b.Votes)
+            .HasForeignKey(v => v.BlogId)
+            .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
