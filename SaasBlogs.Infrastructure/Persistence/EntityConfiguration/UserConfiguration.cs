e240c395160ad05dcaed3751853bd63e855a844c
Awix002 <abipradhanang2014@gmail.com>
2024-04-29 11:13:20 +0545

    Added user configuration

---
+++ SaasBlogs.Infrastructure/Persistence/EntityConfiguration/UserConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SaasBlogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Infrastructure.Persistence.EntityConfiguration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.FirstName)
+                .IsRequired();
+
+            builder.Property(u => u.LastName)
+                .IsRequired();
+
+            builder.Property(u => u.CreatedDate)
+                .IsRequired();
+
+            builder.Property(u => u.LastModifiedDate)
+                .IsRequired(false);
+
+            builder.HasMany(u => u.Blogs)
+                .WithOne(b => b.User)
+                .HasForeignKey(b => b.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(u => u.Comments)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(u => u.Notifications)
+                .WithOne(n => n.User)
+                .HasForeignKey(n => n.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Votes)
+            .WithOne(v => v.User)
+            .HasForeignKey(v => v.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        }
+
+    }
+}
