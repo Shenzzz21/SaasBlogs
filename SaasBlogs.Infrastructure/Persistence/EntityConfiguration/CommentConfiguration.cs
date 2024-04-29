a0d78ace342d2d15fab15d70d4cd160be3b4a105
Awix002 <abipradhanang2014@gmail.com>
2024-04-29 12:39:40 +0545

    Add comment configuration

---
+++ SaasBlogs.Infrastructure/Persistence/EntityConfiguration/CommentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SaasBlogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Infrastructure.Persistence.EntityConfiguration
+{
+    internal class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.CommentBody).IsRequired();
+
+            builder.Property(c => c.CreatedDate).IsRequired();
+
+            builder.Property(c => c.ModifiedDate).IsRequired(false);
+
+            builder.HasOne(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(c => c.Blog)
+                .WithMany(b => b.Comments)
+                .HasForeignKey(c => c.BlogId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
