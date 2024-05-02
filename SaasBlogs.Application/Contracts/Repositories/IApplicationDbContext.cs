191d7af7827a10ed91cef640228e461c22054e28
afterSt0rm <138675790+afterSt0rm@users.noreply.github.com>
2024-05-02 13:17:17 +0545

    Create contract for ApplicationDbContext

---
+++ SaasBlogs.Application/Contracts/Repositories/IApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaasBlogs.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Application.Contracts.Repositories
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Blog> Blogs { get; set; }
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Notification> Notifications { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Vote> Votes { get; set; }
+    }
+}
