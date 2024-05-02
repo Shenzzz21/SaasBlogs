d89e114260ce5a0676245340c18d11d080693da8
afterSt0rm <138675790+afterSt0rm@users.noreply.github.com>
2024-05-02 13:26:04 +0545

    Apply migration

--- SaasBlogs.Application/DTOs/Blog/BlogDetailsDto.cs
+++ SaasBlogs.Application/DTOs/Blog/BlogDetailsDto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SaasBlogs.Application.DTOs.Comment;
+using SaasBlogs.Application.DTOs.Vote;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
