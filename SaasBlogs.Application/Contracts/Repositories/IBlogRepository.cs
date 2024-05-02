8e32eb0eb5e1ba52448fc4d94f0f291a43f872b2
Shen <sheneeza.chaudhary@gmail.com>
2024-05-02 21:17:42 +0545

    Created IBlogRepository

---
+++ SaasBlogs.Application/Contracts/Repositories/IBlogRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using SaasBlogs.Application.DTOs.Blog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Application.Contracts.Repositories
+{
+    public interface IBlogRepository
+    {
+        Task<BlogDetailsDto> CreateBlogAsync(BlogCreateRequestDto blogCreateRequestDto, IFormFile image);
+
+        Task<BlogDetailsDto> GetBlogByIdAsync(string id);
+
+        Task<IEnumerable<BlogDetailsDto>> GetAllBlogsAsync();
+
+        Task<BlogDetailsDto> UpdateBlogAsync(string id, BlogUpdateRequestDto blogUpdateRequestDto);
+
+        Task<bool> DeleteBlogAsync(string id);
+
+    }
+}
