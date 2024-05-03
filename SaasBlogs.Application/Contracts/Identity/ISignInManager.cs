3cef592c25807520a30770d887f8dfa463d8ecd1
afterSt0rm <138675790+afterSt0rm@users.noreply.github.com>
2024-05-03 09:30:29 +0545

    Create contracts for authntication

---
+++ SaasBlogs.Application/Contracts/Identity/ISignInManager.cs
@@ -0,0 +1,17 @@
+﻿using SaasBlogs.Application.DTOs.Response;
+using SaasBlogs.Application.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Application.Contracts.Identity
+{
+    public interface ISignInManager
+    {
+        Task<ResponseWithJwtDto> LoginAsync(UserLoginRequestDto userLoginRequestDto);
+
+        Task<ResponseDto> LogOutAsync();
+    }
+}
---
+++ SaasBlogs.Application/Contracts/Identity/IUserManager.cs
@@ -0,0 +1,25 @@
+﻿using SaasBlogs.Application.DTOs.Response;
+using SaasBlogs.Application.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Application.Contracts.Identity
+{
+    public interface IUserManager
+    {
+        Task<ResponseDto> RegisterUserAsync(UserRegisterRequestDTO userRegisterRequestDto);
+
+        Task<IEnumerable<UserDetailsDto>> GetAllUsersAsync();
+
+        Task<UserDetailsDto> UpdateUserAsync(string id, UserUpdateRequestDto userUpdateRequestDto);
+
+        Task<ResponseDto> DeleteUserAsync(string id);
+
+        Task<UserDetailsDto> GetUserByIdAsync(string id);
+
+        Task<bool> ChangePasswordAsync(string userId, ChangePasswordDto changePasswordDto);
+    }
+}
