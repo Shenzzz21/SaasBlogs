fbb2d886de9728220f8333d1ab1d14b430026cb0
afterSt0rm <138675790+afterSt0rm@users.noreply.github.com>
2024-05-03 09:25:50 +0545

    Create ResponseWithJwtDto

---
+++ SaasBlogs.Application/DTOs/Response/ResponseWithJwtDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaasBlogs.Application.DTOs.Response
+{
+    public class ResponseWithJwtDto
+    {
+        public string? Status { get; set; }
+
+        public string? Message { get; set; }
+
+        public string? JWTToken { get; set; }
+
+        public IList<string>? Roles { get; set; } = new List<string>();
+    }
+}
